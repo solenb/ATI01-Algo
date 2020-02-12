a772611169bfe9aed19169c295c107c9b8bb3de4
Votre Nom <Vous@exemple.com>
2020-02-12 16:31:05 +0100

    12/02/20

--- ATI01-Algo/ex1-matrices.c
+++ ATI01-Algo/ex1-matrices.c
@@ -25,8 +25,8 @@ void fill_tab(int** T,int  nb_L,int  nb_C){
 			printf("Adresse [%d][%d] : ",i,j);
 			scanf("%d",&T[i][j]);
 		}
-	printf("\n");
 	}
+	printf("\n");
 }
 
 void prompt_tab(int** T, int nb_L, int nb_C){
@@ -35,8 +35,8 @@ void prompt_tab(int** T, int nb_L, int nb_C){
 		for (j=0; j < nb_C; j++){
 			printf(" %d ",T[i][j]);
 		}
+	printf("\n");	
 	}
-	printf("\n");
 }
 
 void add_tab(int** tabCalc,int** tab, int ** tab2, int nb_L, int nb_C){
@@ -44,12 +44,34 @@ void add_tab(int** tabCalc,int** tab, int ** tab2, int nb_L, int nb_C){
 	for (i=0; i < nb_L; i++){
 		for (j=0; j < nb_C; j++){
 			tabCalc[i][j]= tab[i][j] + tab2[i][j];
-			printf(" %d ",tabCalc[i][j]);
+		}
+	}
+}
+void mul_tab(int** tabCalc,int** tab, int ** tab2, int nb_L, int nb_C){
+	int i,j,k;
+	for (i=0; i < nb_L; i++){
+		for (j=0; j < nb_C; j++){
+			for (k=0; k < nb_C; k++){
+				tabCalc[i][j] += tab[i][k] * tab2[k][j];
+			}
 		}
 	}
 }
 
 
+void trace_tab(int** tab, int nb_L, int nb_C){
+	int i,j;
+	int t=0;
+	for (i=0; i < nb_L; i++){
+		for (j=0; j < nb_C; j++){
+			if (i == j){
+				t += tab[i][j];
+			}
+		}
+	}
+	printf(" La trace résultante de la matrice : %d", t);
+}
+	
 
 int main(){
 
@@ -58,50 +80,50 @@ printf("Que voulez vous faire ?\n- Addition\t\t-> a\n- Multiplication\t-> m\n- A
 scanf("%c",&action);
 getchar();
 
-if((action == 'm') || (action == 'a')){
+if((action == 'm') || (action == 'a') || (action == 'p') || (action == 't')){
 	int** tab;
-	int** tab2;
-	int** tabCalc;
+	printf("Création de la Matrice 1 : \n");
 	printf("Entrer le nombre de lignes : ");
 	scanf("%d",&lignes);
 	printf("Entrer le nombre de colonnes : ");
 	scanf("%d",&colonnes);
-
-////////// Création des 3 matrices
-	tab=create_tab(lignes, colonnes);
-	tab2=create_tab(lignes, colonnes);
-	tabCalc=create_tab(lignes, colonnes);
-////////// Remplissage des mactrices rentées par l'utilisateur
-	fill_tab(tab, lignes, colonnes);
-	fill_tab(tab2, lignes, colonnes);
-////////// Affichage des mactrices rentées par l'utilisateur 
-	printf("Matrice 1 : \n");
-	prompt_tab(tab, lignes, colonnes);
-	printf("Matrice 2 : \n");
-	prompt_tab(tab2, lignes, colonnes);
 	
-	if((action == 'a')){
-		add_tab(tabCalc, tab, tab2, lignes, colonnes);
-		prompt_tab(tabCalc, lignes, colonnes);
-		delete_tab(tab, lignes, colonnes);
-		delete_tab(tab2, lignes, colonnes);
-		delete_tab(tabCalc, lignes, colonnes);
-	};
-	if((action == 'm')){
-		delete_tab(tab, lignes, colonnes);
-		delete_tab(tab2, lignes, colonnes);
-	};
-};
-if(action == 'p'){
-	int** tab;
-	printf("Entrer le nombre de lignes : ");
-	scanf("%d",&lignes);
-	printf("Entrer le nombre de colonnes : ");
-	scanf("%d",&colonnes);
 	tab=create_tab(lignes,colonnes);
 	fill_tab(tab, lignes, colonnes);
+	printf("Matrice 1 : \n");
 	prompt_tab(tab, lignes, colonnes);
-	delete_tab(tab, lignes, colonnes);
-};
 
+	if((action == 'a') || (action == 'm')){
+		int** tab2;
+		int** tabCalc;
+		printf("Création de la Matrice 2 : \n");
+		printf("Entrer le nombre de lignes : ");
+		scanf("%d",&lignes);
+		printf("Entrer le nombre de colonnes : ");
+		scanf("%d",&colonnes);
+		// Appels des fonctions pour créer, remplir et afficher le tableau
+		tab2=create_tab(lignes,colonnes);
+		fill_tab(tab2, lignes, colonnes);
+		printf("Matrice 2 : \n");
+		prompt_tab(tab2, lignes, colonnes);
+		tabCalc=create_tab(lignes,colonnes);
+		if (action == 'a'){
+			// Appels des fonctions pour addictionner les matrices et afficher le résultat
+			add_tab(tabCalc, tab, tab2, lignes, colonnes);
+		}
+		if((action == 'm')){
+			mul_tab(tabCalc, tab, tab2, lignes, colonnes);
+		}
+		printf("Matrice résultant : \n ");
+		prompt_tab(tabCalc, lignes, colonnes);
+		delete_tab(tab2, lignes, colonnes);
+		delete_tab(tabCalc, lignes, colonnes);
+				
+	
+		
+	}
+	if ((action == 't')){
+		trace_tab(tab, lignes, colonnes);
+	}
+	}
 }
