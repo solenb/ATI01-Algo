19b930a27c5edd2da20c15d4e1120894c7c07cef
Votre Nom <Vous@exemple.com>
2020-03-02 14:33:44 +0100

    02/03/20 fill_poly

--- ATI01-Algo/ex3-polynomes.c
+++ ATI01-Algo/ex3-polynomes.c
@@ -5,10 +5,15 @@ int create_poly(int degre){
 	poly = (int*) malloc(degre * sizeof(double*));
 	return poly;
 }
+
 int fill_poly(int degre, int* poly){
 	printf("Degré du polynome : ");
 	scanf("%d",&degre);
-	
+	printf("Rentrez les %d paramètres du polynome : \n", degre);
+	for (i=0; i < degre; i++){
+		scanf("%d", &poly[i]);
+	}
+}	
 
 
 //int prompt_poly(int degre, int* poly){
